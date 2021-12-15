6e4992a49ee53728445dd330c9d241ec946498b0
kangeline <kangeline@lacritz.com>
2021-12-15 17:14:05 +0100

    style changed

--- src/Components/Startup/ListComponent.tsx
+++ src/Components/Startup/ListComponent.tsx
@@ -10,39 +10,39 @@ export default function ListComponent(props:any){
   return (
     <Card sx={{ maxWidth: "90%", margin: "auto ", marginTop: "20px" }}>
     <CardContent>
-      <Typography variant="body2" sx={{ fontSize: 17, fontWeight: "bold"  }}  gutterBottom>
+      <Typography variant="body2" sx={{ fontSize: 19, fontWeight: "bold"  }}  gutterBottom>
         {props.datalist.name}
       </Typography>
       <Grid container >
         <Grid item pr={1}>
-        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+        <Typography sx={{ fontSize: 13 , fontWeight: 400}} color="text.secondary" gutterBottom>
         Founded {new Date(props.datalist.dateFounded).getFullYear()}
       </Typography>
         </Grid>
         <Divider orientation="vertical" flexItem>
         </Divider>
         <Grid item pl={1} pr={1}>
-        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+        <Typography sx={{ fontSize: 13 , fontWeight: 400}} color="text.secondary" gutterBottom>
         {props.datalist.employees} Employees
       </Typography>
         </Grid>
         <Divider orientation="vertical" flexItem>
         </Divider>
         <Grid item pl={1} pr={1}>
-        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+        <Typography sx={{ fontSize: 14 , fontWeight: 400}} color="text.secondary" gutterBottom>
          {props.datalist.totalFunding} <span>&#36;</span>
       </Typography>
         </Grid>
         <Divider orientation="vertical" flexItem>
         </Divider>
         <Grid item pl={1} pr={1}>
-        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+        <Typography sx={{ fontSize: 13 , fontWeight: 400}} color="text.secondary" gutterBottom>
         {props.datalist.currentInvestmentStage} 
       </Typography>
         </Grid>
       </Grid>
       
-      <Typography sx={{ mt:2,fontWeight: "bold" }} variant="body2">
+      <Typography sx={{ mt:2,fontWeight: "500" }} variant="body2">
       
        {props.datalist.shortDescription}
       </Typography>
