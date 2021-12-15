1116c0f5a31091a17ac80d2eb3201aacf343051f
kangeline <kangeline@lacritz.com>
2021-12-15 16:53:50 +0100

    Task completed

--- src/Components/Startup/ListComponent.tsx
+++ src/Components/Startup/ListComponent.tsx
@@ -1,35 +1,53 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import Grid from '@mui/material/Grid';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import './startupListStyle.css';
 export default function ListComponent(props:any){
   return (
-    // <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-    //   <ListItem alignItems="flex-start">
-    //     <ListItemText
-    //       primary="Brunch this weekend?"
-    //       secondary={
-    //         <React.Fragment>
-    //           <Typography
-    //             sx={{ display: 'inline' }}
-    //             component="span"
-    //             variant="body2"
-    //             color="text.primary"
-    //           >
-    //             Ali Connors
-    //           </Typography>
-    //           {props.datalist.shortDescription}
-    //         </React.Fragment>
-    //       }
-    //     />
-    //   </ListItem>
-    //   </List>
-   
-   <div className="list">
-       <p>{props.dataList.name}</p>
-   </div>
+    <Card sx={{ maxWidth: "90%", margin: "auto ", marginTop: "20px" }}>
+    <CardContent>
+      <Typography variant="body2" sx={{ fontSize: 17, fontWeight: "bold"  }}  gutterBottom>
+        {props.datalist.name}
+      </Typography>
+      <Grid container >
+        <Grid item pr={1}>
+        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+        Founded {new Date(props.datalist.dateFounded).getFullYear()}
+      </Typography>
+        </Grid>
+        <Divider orientation="vertical" flexItem>
+        </Divider>
+        <Grid item pl={1} pr={1}>
+        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+        {props.datalist.employees} Employees
+      </Typography>
+        </Grid>
+        <Divider orientation="vertical" flexItem>
+        </Divider>
+        <Grid item pl={1} pr={1}>
+        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+         {props.datalist.totalFunding} <span>&#36;</span>
+      </Typography>
+        </Grid>
+        <Divider orientation="vertical" flexItem>
+        </Divider>
+        <Grid item pl={1} pr={1}>
+        <Typography sx={{ fontSize: 13 , fontWeight: "bold"}} color="text.secondary" gutterBottom>
+        {props.datalist.currentInvestmentStage} 
+      </Typography>
+        </Grid>
+      </Grid>
+      
+      <Typography sx={{ mt:2,fontWeight: "bold" }} variant="body2">
+      
+       {props.datalist.shortDescription}
+      </Typography>
+    </CardContent>
+  </Card>
    
     )
         }
